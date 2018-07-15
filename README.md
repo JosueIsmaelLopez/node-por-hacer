1cb2be504f5d37ae9d6c28299f056c0593495854
Josue <josuekuroko@gmail.com>
2018-07-14 20:18:14 -0600

    Primer Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+##Aplicacion de comandos
+
+Ejercicio del curso
+
+Recuerden instalar los paquetes de node
+
+
+npm install
---
+++ config/yargs.js
@@ -0,0 +1,29 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripcion de la tarea por hacer'
+};
+
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completado la tarea por hacer'
+}
+
+const argv = require('yargs')
+    .command('crear', 'Crea una tarea por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualiza una tarea por hacer', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Borra una tarea ya realizada', {
+        descripcion
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
